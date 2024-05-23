b55eb235103b7b29c3eeb0c69e622a083e7c69c8
Endo Axelsson <endo.axelsson@elev.ga.ntig.se>
2024-05-23 11:18:02 +0200

    dok + cart

--- dokumentation/README.md
+++ dokumentation/README.md
@@ -3,14 +3,28 @@
 
  ### Dokumentaion
 
+Startade med att forka ALC-login projektet på github
+Installerade monsnode och fixade env så att den kan använda sig av tableplus
+ + gitignore för att ta bort kritisk information
+tog bort onödiga njk och kod som inte behövdes för arbetet
 
+##
+Nav baren fick ytterligare 2 nya features:
+En varukorg där en ny njk länkades och detta gjordes om till en bild
+Nav gjordes till sticky, den följer med skärmen ner när det skrollas
 
- Gjorde bilder med länk i nav
+##
+ skapade 2 nya tabeller i tableplus:
+ endo_produkts - innehåller produkternas namn, pris och bild
+ Endo_login för inloggning (id, username, password )
+ + gjorde så att password blev gömd
 
- 2 tabeller i tableplus.
- En för inloggning och andra för produkter (namn, id, pris, bild)
+ ##
+ Utifrån databasen gjordes en for product loop så att varje produkt skapas utifrån en mall
+Bild, namn, pris och en knapp
+För varje knapp skapades en nya id
 
-
+##
 Gjorde så att button räknar så länge produkten finns i korgen.
 använde en find produkt som sedan letade efter id
 löstes genom att gör en if sats  där den kollade om produkt fanns
@@ -19,5 +33,14 @@ om inte lägg till den med 1
 visade undifnied - löste genom att lägga till en nya rad och flytta upp den
 
 
-Påbörja varukorgen genom att göra en lista
+Påbörja varukorgen genom att göra en lista för varje produkt
 produkt ska kunnas ta bort och köper man så läggs den i databasen.
+
+## 
+I varukorgen ska 'cart' nås
+Detta gjordes genom att skapa en ul med klassen product-list
+utifrån det skapades en js skript som selectade allt i product-list
+En let cart las även till för att hämta items från localstoragen ('cart') 
+i if (cart) loopen så skapas en ny element som skapar en li
+av det görs en appendChild av li
+Problem nu är att det som finns i carten är undefined!
--- public/js/button.js
+++ public/js/button.js
@@ -1,30 +1,26 @@
 window.addEventListener('load', (event) => {
     // När jag clickar så ska jag få ut vilken produkt jag väljer (id, namn, antal)
     // Detta ska värderna ska sparas i local storage
-
     const addToCartButtons = document.querySelectorAll('.addtocardbutton');
-
 
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
     console.log(cart)
-
-
 
     addToCartButtons.forEach(addToCartButton => {
         addToCartButton.addEventListener('click', (event) => {
             event.preventDefault();
             console.log(addToCartButton) // häm,ta data värdet för produkten   
-            cart.push({ id: addToCartButton.dataset.product, count: 1 })
             const product = cart.find(product => product.id === addToCartButton.dataset.product)
-            console.log(product) // vad händer om produkten inte är i localstorage ännu
-            
+            //cart.push({ id: addToCartButton.dataset.product, count: 1 })
             
             if (product) {
                 product.count++
             } else {
                 cart.push({ id: addToCartButton.dataset.product, count: 1 })
             }
-
+            const product2 = cart.find(product => product.id === addToCartButton.dataset.product)
+            console.log(product2) // vad händer om produkten inte är i localstorage ännu
+            
             // om profukten redan finns i cart, öka count
             // cart.find du har id på produkten sök efter id i cart
             // annars så lägg till den 
@@ -32,21 +28,4 @@ window.addEventListener('load', (event) => {
             localStorage.setItem('cart', JSON.stringify(cart)) // spara i localstorage
         });
     })
-
-
-    return
-    console.log(result); // { name: 'cherries', quantity: 5 }
-
-
-
-
-    addToCartButtons.forEach(addToCartButton => {
-        addToCartButton.addEventListener('click', (event) => {
-            event.preventDefault();
-            console.log(addToCartButton) // häm,ta data värdet för produkten    
-            cart.push({ id: addToCartButton.dataset.product, count: 1 }) // lägg till produkt
-            localStorage.setItem('cart', JSON.stringify(cart)) // spara i localstorage
-        });
-    })
-
 });
